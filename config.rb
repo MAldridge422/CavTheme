bfb46298396dbc91e47e0cc85e0449db99de9b76
MAldridge422 <Meece@d-172-25-98-187.bootp.Virginia.EDU>
2015-07-04 14:27:49 -0400

    Edited config.rb for production
    Turned off line_comments
    Changed output_style to :compressed

--- config.rb
+++ config.rb
@@ -1,14 +1,16 @@
 http_path = "/" #root level target path
-css_dir = "." #targets our default style.css file at the root level of our theme
-sass_dir = "sass" #targets our sass directory
-images_dir = "images" #targets our pre existing image directory
-javascripts_dir = "js" #targets our JavaScript directory
- 
-# You can select your preferred output style here (can be overridden via the command line):
-output_style = :nested
-# change to :compressed during output
- 
+css_dir = "." #targets default style.css file at the root level of our theme
+sass_dir = "sass" #targets sass directory
+images_dir = "images" #targets image directory
+javascripts_dir = "js" #targets JavaScript directory
+
+# use :nested during development, :compressed during production 
+output_style = :compressed
+
+# Prints comments detailing what .scss line created a piece of the .css file
+# use true during development, false during prouction
+line_comments = false
+
 # To enable relative paths to assets via compass helper functions.
 # note: this is important in wordpress themes for sprites
- 
 relative_assets = true
